06aedd4988978928123f0c91b4c2f91db91fcf42
Kyle Matthew Carter <kmcarter91@service0.ice.americas.sgi.com>
2015-11-17 20:11:10 -0700

    blast

---
+++ hpc-blast/scripts/01-bio-searchio.pl
@@ -0,0 +1,99 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
+use Getopt::Long;
+use Pod::Usage;
+use Bio::SearchIO;
+
+my %opts = get_opts();
+my @args = @ARGV;
+
+if ($opts{'help'} || $opts{'man'}) {
+    pod2usage({
+        -exitval => 0,
+        -verbose => $opts{'man'} ? 2 : 1
+    });
+}
+
+my $infile=shift @args;
+my $indata=new Bio::SearchIO(-format => 'blast',
+                             -file   => $infile,
+                            );
+say("query\thit\tevalue");
+
+my $min = 1e-10;
+while( my $current = $indata->next_result){
+    while( my $hit = $current->next_hit){
+        while( my $hsp = $hit->next_hsp){
+            if($hsp->evalue <= $min){
+                print($current->query_name,"\t");
+                print($hit->name,"\t");
+                print($hit->description, "\t");
+                print($hsp->percent_identity, "\t");
+                print($hsp->length('total'),"\t");
+                say($hsp->evalue);
+            }
+        }
+    }
+}
+
+#say "OK";
+
+# --------------------------------------------------
+sub get_opts {
+    my %opts;
+    GetOptions(
+        \%opts,
+        'help',
+        'man',
+    ) or pod2usage(2);
+
+    return %opts;
+}
+
+__END__
+
+# --------------------------------------------------
+
+=pod
+
+=head1 NAME
+
+01-bio-searchio.pl
+
+=head1 SYNOPSIS
+
+  01-bio-searchio.pl query_v_sprot.blastout
+
+Options:
+
+  --help   Show brief help and exit
+  --man    Show full documentation
+
+=head1 DESCRIPTION
+
+Parses Blast output using "Bio::SearchIO" and retrieves HSP for hits with significance less than or equal to 1x10^-50.
+
+
+=head1 SEE ALSO
+
+perl.
+
+=head1 AUTHOR
+
+Kyle Matthew Carter E<lt>kmcarter91@email.arizona.eduE<gt>.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2015 kmcarter91
+
+This module is free software; you can redistribute it and/or
+modify it under the terms of the GPL (either version 1, or at
+your option, any later version) or the Artistic License 2.0.
+Refer to LICENSE for the full license text and to DISCLAIMER for
+additional warranty disclaimers.
+
+=cut
